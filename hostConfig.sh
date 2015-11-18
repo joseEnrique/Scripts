e7ea283f61eb7f7585fa02eaded08075c54b95c3
joseEnrique <joseenriqueruiznavarro@gmail.com>
2015-11-18 10:10:03 +0100

    first script

---
+++ hostConfig.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#Read every databases from mysql and check if exists in some file /home/x/public
+for OUTPUT in $(mysql -e 'show databases')
+do
+  	IFS='_' read -a name <<< "$OUTPUT"
+        if [ -n "$(find /home/"$name"/public_html -type f -exec grep -l "$O$
+        then
+	echo "$OUTPUT" has a config file >>data.txt
+        else
+	echo "$OUTPUT" HASN'T  has config file  >>data.txt
+
+        fi
+done
+
